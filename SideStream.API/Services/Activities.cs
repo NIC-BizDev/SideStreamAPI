8e9f969a684b0ae4aca0d25bf955819024e382b8
danny <danny@egovmt.com>
2015-04-11 15:30:08 -0400

    Add Get Activities to API
    Adds the stuff for getting activities/events/tours based on location.

---
+++ SideStream.API/Services/Activities.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SideStream.API.Services
+{
+    public static class Activities
+    {
+        public static string Get(string lat, string lon, string radius)
+        {
+            return GetActivities(lat, lon, radius);
+        }
+        public static string GetActivities(string lat, string lon, string radius)
+        {
+            var events = new Dictionary<string, JToken>();
+            var activities = new Dictionary<string, JToken>();
+            var tours = new Dictionary<string, JToken>();
+
+            foreach (var recAreaId in JObject.Parse(RIDB.GetRecAreas(lat, lon, radius)).Children()["RecAreaID"].ToList().Select(j => j.ToString()))
+            {
+                AddItemsUnlessThere(events,     RIDB.GetRecAreaEvents(recAreaId),     "EventID"   );
+                AddItemsUnlessThere(activities, RIDB.GetRecAreaActivities(recAreaId), "ActivityID");
+            }
+            foreach (var facilityId in JObject.Parse(RIDB.GetFacilities(lat, lon, radius)).Children()["RecAreaID"].ToList().Select(j => j.ToString()))
+            {
+                AddItemsUnlessThere(events,     RIDB.GetFacilityEvents(facilityId),     "EventID");
+                AddItemsUnlessThere(activities, RIDB.GetFacilityActivities(facilityId), "ActivityID");
+                AddItemsUnlessThere(tours,      RIDB.GetFacilityTours(facilityId),      "TourID");
+            }
+
+            var retval = new
+            {
+                Events = events.Values.Select(e => e as JObject).ToList(),
+                Activities = activities.Values.Select(a => a as JObject).ToList(),
+                Tours = tours.Values.Select(t => t as JObject).ToList(),
+            };
+            return JsonConvert.SerializeObject(retval, Formatting.Indented);
+        }
+
+        private static void AddItemsUnlessThere(Dictionary<string, JToken> dict, string json, string key) 
+        {
+            foreach (var thing in JObject.Parse(json).Children().ToList())
+            {
+                AddUnlessThere(dict, thing[key].ToString(), thing);
+            }
+        }
+
+        private static void AddUnlessThere(Dictionary<string, JToken> dict, string key, JToken value)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, value);
+            }
+        }
+    }
+}
---
+++ SideStream.API/Services/RIDB.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SideStream.API.Services
+{
+    public static class RIDB
+    {
+        private static string _ridbBaseurl = ConfigurationManager.AppSettings["RIDBBaseUrl"];
+        private static string _ridbVersion = ConfigurationManager.AppSettings["RIDBVersion"];
+        private static string _ridbApiKey = ConfigurationManager.AppSettings["RIDBkey"];
+
+        public static string GetRecAreas(string lat, string lon, string radius)
+        {
+            var parameters = new Dictionary<string, string>() {
+                { "latitude", lat },
+                { "longitude", lon },
+                { "radius", radius }
+            };
+            return CallRIDB(parameters, "recareas");
+        }
+
+        public static string GetRecAreaActivities(string recAreaId)
+        {
+            return CallRIDB("recareas", recAreaId, "activities");
+        }
+
+        public static string GetRecAreaEvents(string recAreaId)
+        {
+            return CallRIDB("recareas", recAreaId, "events");
+        }
+
+        public static string GetFacilities(string lat, string lon, string radius)
+        {
+            var parameters = new Dictionary<string, string>() {
+                { "latitude", lat },
+                { "longitude", lon },
+                { "radius", radius }
+            };
+            return CallRIDB(parameters, "facilities");
+        }
+
+        public static string GetFacilityActivities(string facilityId)
+        {
+            return CallRIDB("facilities", facilityId, "activities");
+        }
+
+        public static string GetFacilityEvents(string facilityId)
+        {
+            return CallRIDB("facilities", facilityId, "events");
+        }
+
+        public static string GetFacilityTours(string facilityId)
+        {
+            return CallRIDB("facilities", facilityId, "tours");
+        }
+
+        public static string CallRIDB(params string[] pieces)
+        {
+            return CallRIDB(pieces);
+        }
+        public static string CallRIDB(Dictionary<string, string> parameters, params string[] pieces)
+        {
+            var url = GetRidbUrl(pieces);
+            var client = new WebClient();
+            client.Headers.Add("apikey", _ridbApiKey);
+            return client.DownloadString(url);
+        }
+
+        public static string GetRidbUrl(params string[] pieces)
+        {
+            return String.Format("https://{0}/{1}/{2}", _ridbBaseurl, _ridbVersion, String.Join("/", pieces));
+        }
+
+    }
+}
