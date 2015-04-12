9022187f452d48b7ad62b44792cefa604c854ad0
shaunakv1 <shaunakv1@gmail.com>
2015-04-12 14:56:12 -0400

    tips retrival query online!

--- SideStream.HtmlDemo/js/tips.main.js
+++ SideStream.HtmlDemo/js/tips.main.js
@@ -3,5 +3,33 @@ $(function(){
 
 	$('#searchRidb').click(function (e) {
 		var id = $("#ridbID").val();
+
+		$.ajax({
+			url: 'http://ridb-tips.elasticbeanstalk.com/tour/'+id+'/tips',
+			dataType: 'json'
+		})
+		.done(function(tips) {
+			console.log(tips);
+
+			tips.forEach(function (tip, i) {
+				$('#tips').append(
+						'<div class="row">'+
+						    '<div class="large-24 columns">'+
+						        '<div class="panel">'+
+
+						            '<i class="step fi-heart size-21"></i>'+
+						            '<i class="step fi-heart size-21"></i>'+
+						            '<i class="step fi-heart size-21"></i>'+
+						            '<i class="step fi-heart size-21"></i>'+
+						            '<i class="step fi-heart size-21"></i>'+
+						            '<div>'+
+						                tip.description+
+						            '</div>'+
+						        '</div>'+
+						    '</div>'
+				);
+			});
+		});
 	})
+	
 });
